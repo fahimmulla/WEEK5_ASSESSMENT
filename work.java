159d6a05324953c2e4b7d1bebd25aea5b92d2312
fahimmulla <fahim22@hotmail.co.uk>
2018-04-06 12:33:30 +0000

    Friday

---
+++ work.java
@@ -0,0 +1,13 @@
+class work {
+   public static void main(String[] args) {
+      int number = 10;
+      if (number > 0) {
+         System.out.println("Number is positive.");
+      }
+      else {
+         System.out.println("Number is not positive.");
+      }
+ 
+      System.out.println("This statement is always executed.");
+   }
+}
